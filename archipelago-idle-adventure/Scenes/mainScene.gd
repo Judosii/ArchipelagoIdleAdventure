2443dbe87baf9a8f759bc80be7356c3cb9d813c0
Judosii <skutnik.martin@gmail.com>
2024-11-27 17:25:09 +0100

    levels can be loaded ! make the level editor.

--- archipelago-idle-adventure/Scenes/mainScene.gd
+++ archipelago-idle-adventure/Scenes/mainScene.gd
@@ -21,31 +21,34 @@ func unloadLevel():
 
 func loadLevel(levelName : String):
 	unloadLevel()
-	var levelPath = "res://Levels/%s.tscn" % levelName
+	var levelPath = "res://Levels/%s" % levelName
 	var levelResource := load(levelPath)
 	if levelResource :
-		levelInstance = levelResource.instance()
+		levelInstance = levelResource.instantiate()
+		main2D.add_child(levelInstance)
+		print(levelInstance)
 
 func LevelChosen(levelPath : String):
 	chosenLevel = load(levelPath)
 
 func _on_play_level_button_down() -> void:
 	#Can only be used on main menu
-	loadLevel(chosenLevel.name)
+	SetLayout()
 
 func _on_level_editor_button_down():
 	#Can only be used on main menu
 	#Open the level editor with levelInstance
-	setLayout()
 	print("bbb")
 
 # Followed this tutorial for file exploring :
 # https://youtu.be/mC4Wb_NHKA8?si=qVmsGqYBg_OVUTKQ
+
 @export var cont :Container
 var path :String = ""
 var file : bool = true
 
-func setLayout():
+func SetLayout():
+	mainMenu.visible = false
 	file = false
 	for i in cont.get_children():
 		i.queue_free()
@@ -53,20 +56,20 @@ func setLayout():
 	dir.list_dir_begin()
 	var fileName = dir.get_next()
 	while fileName != "":
-		var nBut = Button.new()
-		nBut.text = fileName
-		cont.add_child(nBut)
-		if dir.current_is_dir():
-			#It's a folder. Filter this out.
-			pass
-		else:
-			nBut.pressed.connect(FileChosen.bind(fileName))
+		if fileName.get_extension() == "tscn":
+			var nBut = Button.new()
+			nBut.text = fileName
+			cont.add_child(nBut)
+			nBut.pressed.connect(loadLevel.bind(fileName))
 		fileName = dir.get_next()
 
 func FileChosen(fileName : String):
+	chosenLevel = load("res://Levels/%s"% fileName)
 	if file :
 		path = path.get_base_dir()
 	path = path + "/" + fileName
 	#NPath.text // Npath is not used! it's a lineEdit to show the path.
 	#as we are always in res://Levels, it's not used.
 	file = true
+	#print(chosenLevel)
+	loadLevel(chosenLevel.resource_name)

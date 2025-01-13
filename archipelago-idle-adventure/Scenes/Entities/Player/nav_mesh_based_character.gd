06612dfbd0d3122f661891fa199a9d73c00f536b
Judosii <skutnik.martin@gmail.com>
2025-01-13 13:18:06 +0100

    tilemap collisions work

--- archipelago-idle-adventure/Scenes/Entities/Player/nav_mesh_based_character.gd
+++ archipelago-idle-adventure/Scenes/Entities/Player/nav_mesh_based_character.gd
@@ -3,14 +3,15 @@ extends CharacterBody2D
 var speed = 300
 var accel = 7
 
-@export var dest : Node2D
+@export var dest : Array[Node2D]
+var index : int
 @onready var nav : NavigationAgent2D = $NavigationAgent2D
 
 
 func _physics_process(delta):
 	var direction = Vector3()
 	
-	nav.target_position = dest.global_position
+	nav.target_position = dest[index].global_position
 	
 	direction = nav.get_next_path_position() - global_position
 	direction = direction.normalized()
@@ -19,4 +20,4 @@ func _physics_process(delta):
 
 
 func _on_navigation_agent_2d_navigation_finished():
-	process_mode = PROCESS_MODE_DISABLED
+	index+=1

58b48f6ebfa52a4f505ec40478ddb5e9f6ea2ead
mskutnik <m.skutnik@rubika-edu.com>
2025-01-14 16:18:54 +0100

    restart from 0. Make docs, make everything using NavMesh

--- archipelago-idle-adventure/Engine/MainSceneStuff/RecentLevels.gd
+++
@@ -1,4 +0,0 @@
-class_name recentLevels extends Resource
-
-@export var levelName : Array[String]
-@export var levelpath : Array[String]
--- archipelago-idle-adventure/Scenes/MovementNodes/movement_node.gd
+++
@@ -1,8 +0,0 @@
-class_name MovementNodes extends Node2D
-
-@export var connectedNodes : Array[MovementNodes]
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.get_parent() is character:
-		var chara : character = area.get_parent()
-		chara.ArrivedAtNode()

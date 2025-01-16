3db5d59ea84bc070dd7a09b9a1124c45ea61e0c2
mskutnik <m.skutnik@rubika-edu.com>
2025-01-16 19:43:05 +0100

    character moves towards mouse.

--- archipelago-idle-adventure/Scenes/Characters/test1/NavMeshCharacter.gd
+++ archipelago-idle-adventure/Scenes/Characters/test1/NavMeshCharacter.gd
@@ -1,9 +1,16 @@
 class_name NavMeshCharacter extends CharacterBody2D
 
+signal moving
+signal foundItem
+
+@export var active : bool
+@export var speed = 300
+
+@export_category("Nodes")
+
 @export var nav : NavigationAgent2D
-var speed = 300
+@export var anim : AnimationPlayer
 var accel = 7
-var active : bool
 
 func _physics_process(delta):
 	if active:
@@ -16,3 +23,17 @@ func _physics_process(delta):
 		
 		velocity = velocity.lerp(direction * speed, accel * delta)
 		move_and_slide()
+		moving.emit()
+	else:
+		anim.play("Idle")
+
+func _on_moving() -> void:
+	if velocity != Vector2.ZERO && anim.current_animation == "Walk":
+		return
+	anim.play("Walk")
+
+func ReceivedItem():
+	anim.play("ReceivedItem")
+
+func FoundItem():
+		anim.play("SendingItem")

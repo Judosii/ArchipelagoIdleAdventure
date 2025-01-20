0dfad758f52954ebd1a5c84dcb6c18a6d42e9591
Judosii <skutnik.martin@gmail.com>
2025-01-20 16:15:12 +0100

    to work from home

---
+++ archipelago-idle-adventure/Scripts/Engine/cameraArea.gd
@@ -0,0 +1,14 @@
+class_name AreabasedCamera extends Camera2D
+
+func OnNewRoomEntered(area :Area2D): #Set limits of camera to be the room's area.
+	var collShape : CollisionShape2D = area.get_child(0)
+	var size = collShape.shape.size * 2
+	var viewSize = get_viewport_rect().size
+	
+	if size.y < viewSize.y : size.y = viewSize.y
+	if size.x < viewSize.x : size.x = viewSize.x
+	
+	limit_top = collShape.global_position.y - size.y /2
+	limit_left = collShape.global_position.x - size.x /2
+	limit_bottom = limit_top + size.y 
+	limit_right = limit_left+ size.x  

cb946b5d25dcc7bc39c27e88b41157d993eb484f
Judosii <skutnik.martin@gmail.com>
2025-01-21 15:52:55 +0100

    a

--- archipelago-idle-adventure/Scenes/Characters/test1/NavMeshCharacter.gd
+++ archipelago-idle-adventure/Scenes/Characters/test1/NavMeshCharacter.gd
@@ -6,8 +6,10 @@ signal foundItem
 @export var active : bool
 @export var speed = 300
 
-@export_category("Nodes")
+@export var list : Array[Node2D]
+var i: int = 0
 
+@export_category("Nodes")
 @export var nav : NavigationAgent2D
 @export var anim : AnimationPlayer
 var accel = 7
@@ -16,7 +18,8 @@ func _physics_process(delta):
 	if active:
 		var direction = Vector3()
 		
-		nav.target_position = get_global_mouse_position()
+		#nav.target_position = get_global_mouse_position()
+		nav.target_position = list[i].global_position
 		
 		direction = nav.get_next_path_position() - global_position
 		direction = direction.normalized()
@@ -24,6 +27,10 @@ func _physics_process(delta):
 		velocity = velocity.lerp(direction * speed, accel * delta)
 		move_and_slide()
 		moving.emit()
+		if position.distance_squared_to(list[i].global_position) < 10 && i > 4:
+			i +=1
+		else: 
+			active = false
 	else:
 		anim.play("Idle")
 
@@ -37,13 +44,3 @@ func ReceivedItem():
 
 func FoundItem():
 		anim.play("SendingItem")
-
-func EnteredCameraArea():
-	pass
-	# Enter camera transition; stop moving
-	# Get camera
-	# Get camera position
-	# Set camera bounds to unbound
-	# Tween camera position to next position (nextDir.x * cam.Size.x / 2, nextDir.y * cam.Size.y / 2)
-	# Set camera bounds again
-	# Resume movement
